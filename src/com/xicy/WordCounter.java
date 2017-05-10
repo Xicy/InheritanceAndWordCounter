c9549f96744307b885a249cefdca968d8920404e
Umut Akkaya <umutakkaya1996@gmail.com>
2017-05-10 21:31:17 +0300

    a

--- src/com/xicy/WordCounter.java
+++ src/com/xicy/WordCounter.java
@@ -1,5 +1,9 @@
 package com.xicy;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TreeMap;
@@ -11,24 +15,20 @@ public class WordCounter {
     private HashMap<String, Integer> set;
     private int totalCount;
 
-    public WordCounter() {
-        reset();
-    }
-
-    public void initialize(String text) {
-        if (set.size() > 0) throw new ExceptionInInitializerError("use reset method");
-        for (String word : text.replaceAll("\n", "").toLowerCase().split("\\s")) {
-            word = word.trim();
-            if (!word.isEmpty()) {
-                set.put(word, set.getOrDefault(word, 0) + 1);
-                totalCount++;
-            }
-        }
-    }
-
-    public void reset() {
+    public WordCounter(String path) {
         set = new HashMap<>();
         totalCount = 0;
+        try {
+            for (String word : new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8).replaceAll("\n", "").toLowerCase().split("\\s")) {
+                word = word.trim();
+                if (!word.isEmpty()) {
+                    set.put(word, set.getOrDefault(word, 0) + 1);
+                    totalCount++;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public int getTotalCount() {
--- src/com/xicy/WordCounterTest.java
+++ src/com/xicy/WordCounterTest.java
@@ -7,12 +7,11 @@ import java.util.Map;
  */
 public class WordCounterTest {
     public static void main(String[] args) {
-        WordCounter wc = new WordCounter();
-        wc.initialize("So close no matter how far \n Couldn't be much more from the heart \n Forever trusting who we are \n And nothing else matters \n Never opened myself this way \n Life is outs we live it our way \n All these words I don't just say \n And nothing else matters \n\nTrust I seek and I find in you \n Every day for us something new \n Open mind for a different view \n And nothing else matters \n \n Never cared for what they do \n Never cared for what they know\n But I know");
+        WordCounter wc = new WordCounter("sample.txt");
 
         for (Map.Entry<String, Integer> entry : wc.getEntries().entrySet())
             System.out.print(entry.getKey() + "=" + entry.getValue() + ", ");
-        System.out.println("\n" + wc.getDistinctCount());
-        System.out.println(wc.getTotalCount());
+        System.out.println("\nTotal dis count:" + wc.getDistinctCount());
+        System.out.println("Total word count" + wc.getTotalCount());
     }
 }
