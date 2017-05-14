70d93ba76a753fa69b0923426f3c5f6f6d277c11
Umut Akkaya <umutakkaya1996@gmail.com>
2017-05-15 01:46:16 +0300

    added file chooser

--- src/com/xicy/WordCounterTest.java
+++ src/com/xicy/WordCounterTest.java
@@ -1,17 +1,28 @@
 package com.xicy;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.util.Map;
 
 /**
  * Created by Umut Akkaya on 7.05.2017.
  */
 public class WordCounterTest {
-    public static void main(String[] args) {
-        WordCounter wc = new WordCounter("sample.txt");
+    public static void main(String[] args) throws Exception {
+        UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        JFileChooser fileChooser = new JFileChooser();
+        fileChooser.setFileFilter(new FileNameExtensionFilter("Sample Text File", "txt", "text"));
+        int result;
 
-        for (Map.Entry<String, Integer> entry : wc.getEntries().entrySet())
-            System.out.print(entry.getKey() + "=" + entry.getValue() + ", ");
-        System.out.println("\nTotal dis count:" + wc.getDistinctCount());
-        System.out.println("Total word count" + wc.getTotalCount());
+        do {
+            if ((result = fileChooser.showOpenDialog(null)) == JFileChooser.APPROVE_OPTION) {
+                WordCounter wc = new WordCounter(fileChooser.getSelectedFile().getAbsolutePath());
+                for (Map.Entry<String, Integer> entry : wc.getEntries().entrySet())
+                    System.out.print(entry.getKey() + "=" + entry.getValue() + ", ");
+                System.out.println("\nTotal dis count:" + wc.getDistinctCount());
+                System.out.println("Total word count" + wc.getTotalCount());
+            }
+        } while (result != JFileChooser.APPROVE_OPTION);
+
     }
 }
